bfeeba68771cf26fdf6b8f40dd2f6cefae8f78fc
vassin <vassin@kolesa.kz>
2023-05-14 17:48:24 +0600

    First Commit

--- src/views/IndexViews/IndexViews.tsx
+++ src/views/IndexViews/IndexViews.tsx
@@ -1,11 +1,30 @@
 import React, { FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '@/store/module/counter';
 
 import styles from './indexViews.module.scss';
 
 export const IndexViews: FC = () => {
+    const count = useSelector((state: { counter: CounterType }) => state.counter.value);
+    const dispatch = useDispatch();
+
     return (
         <div className={styles['index-views']}>
-            Hello World
+            <button
+                type="button"
+                onClick={() => dispatch(increment())}
+            >
+                +
+            </button>
+            <span>
+                { count }
+            </span>
+            <button
+                type="button"
+                onClick={() => dispatch(decrement())}
+            >
+                -
+            </button>
         </div>
     )
 };
